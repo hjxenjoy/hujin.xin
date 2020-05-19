b6366d84d5e33c9c70e2b0f2d44974cf7122623b
Jinxin Hu <hjxenjoy@gmail.com>
2020-05-19 09:40:55 +0800

    feat(ga): add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import PageFooter from '../components/PageFooter'
 
+const googleTags = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-167052593-1');
+`
+const isProd = process.env.NODE_ENV === 'production'
+
 export default class MyDocument extends Document {
   // static async getInitialProps(ctx) {
   //   const initialProps = await Document.getInitialProps(ctx)
   //   return { ...initialProps }
   // }
 
+  /* eslint-disable react/no-danger */
   render() {
     return (
       <Html lang="zh-cmn-Hans">
@@ -21,8 +30,18 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           <PageFooter />
+          {isProd && (
+            <>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=UA-167052593-1"
+              />
+              <script dangerouslySetInnerHTML={{ __html: googleTags }} />
+            </>
+          )}
         </body>
       </Html>
     )
+    /* eslint-enable react/no-danger */
   }
 }
